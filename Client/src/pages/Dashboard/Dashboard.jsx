f0a2f3a2d54833f9ee6ad045c44df9ed9c6e958e
keshavk27 <keshavk.2027@gmail.com>
2026-07-08 19:17:12 +0530

    import fix

--- Client/src/pages/Dashboard/Dashboard.jsx
+++ Client/src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import WelcomeScreen from "../../components/chat/WelcomeScreen.jsx";
+import WelcomeScreen from "../../components/chat/WelcomeScreen.jsx"; 
 import ChatInput from "../../components/chat/ChatInput.jsx";
 import ChatWindow from "../../components/chat/ChatWindow.jsx";
 import { getChatSessionByIdThunk } from "../../features/chatsession/chatSessionThunk.js";
